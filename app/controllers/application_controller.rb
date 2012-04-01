bf1414d4af1352326dbb9520725a9a9cc49339dd
Pierre <pierre.coetzee@gmail.com>
2012-04-01 22:41:25 +0100

    add some test data for when the launch the app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :check_users
+  
+  def check_users
+      if User.all.length==0
+       User.create(:user_name => "Test person")       
+       User.last.tasks.push Task.new(:name => "Do some cool task", :completion_date => Date.today.advance(:days=>7) )
+       User.last.tasks.push Task.new(:name => "Do some cool task", :completion_date => Date.today.advance(:days=>-17) )
+       User.last.tasks.push Task.new(:name => "Do some cool task", :completion_date => Date.today )
+       User.last.save
+       
+    end
+  end
 end
--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,6 +1,5 @@
 class TaskController < ApplicationController
-  def index
-    throw "There are no users in your Mongo database - please run rake db:seed" if User.all.length==0
+  def index   
     @user = User.first
   end
 
